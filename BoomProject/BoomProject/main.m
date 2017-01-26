253df799415d3e6598ab5174146c5d145f43ba39
Mikayel Sahakyan <mikayel92@gmail.com>
2017-01-26 15:28:51 +0400

    Created a new project

---
+++ BoomProject/BoomProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BoomProject
+//
+//  Created by User ACA on 1/26/17.
+//  Copyright © 2017 Mikayel Sahakyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
